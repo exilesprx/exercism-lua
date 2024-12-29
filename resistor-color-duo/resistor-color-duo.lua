9030c9b4eaf857198aceb1234233601034276753
Exiles.prx <exiles.prx@gmail.com>
2024-12-28 19:54:26 -0500

    Solved exericse resistor color duo

--- resistor-color-duo/resistor-color-duo.lua
+++ resistor-color-duo/resistor-color-duo.lua
@@ -1,4 +1,18 @@
+local resistors = {
+	black = 0,
+	brown = 1,
+	red = 2,
+	orange = 3,
+	yellow = 4,
+	green = 5,
+	blue = 6,
+	violet = 7,
+	grey = 8,
+	white = 9,
+}
+
 return {
-  value = function(colors)
-  end
+	value = function(colors)
+		return tonumber(resistors[colors[1]] .. resistors[colors[2]])
+	end,
 }
